faa04d9fc7dcbbeb12211eebcac1642c5371ed30
Matthew Mauney <mauney@users.noreply.github.com>
2020-03-29 23:29:59 -0400

    Try two plots in one script

--- my_dsfs/ch03_visualization.py
+++ my_dsfs/ch03_visualization.py
@@ -11,8 +11,25 @@ plt.title("Nominal GDP")
 
 # add a label to the y-axis
 plt.ylabel("Billions of $")
-# plt.show()
+plt.show()
+
+# plt.savefig('im/viz_gdp.png')
+plt.gca().clear()
 
 
-plt.savefig('im/viz_gdp.png')
+movies = ["Annie Hall", "Ben-Hur", "Casablanca", "Gandhi", "West Side Story"]
+num_oscars = [5, 11, 3, 8, 10]
+
+# plot bars with left x-coordinates [0, 1, 2, 3, 4], heights [num_oscars]
+plt.bar(range(len(movies)), num_oscars)
+
+plt.title("My Favorite Movies")     # add a title
+plt.ylabel("# of Academy Awards")   # label the y-axis
+
+# label x-axis with movie names at bar centers
+plt.xticks(range(len(movies)), movies)
+
+plt.show()
+
+# plt.savefig('im/viz_movies.png')
 plt.gca().clear()
