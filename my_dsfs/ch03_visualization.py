40bbbae24533095a232d4877e56cdfc22043fe00
mauney <15823766+mauney@users.noreply.github.com>
2020-04-23 23:32:15 -0400

    Start histogram

--- my_dsfs/ch03_visualization.py
+++ my_dsfs/ch03_visualization.py
@@ -33,3 +33,17 @@ plt.show()
 
 # plt.savefig('im/viz_movies.png')
 plt.gca().clear()
+
+from collections import Counter
+grades = [83, 95, 91, 87, 70, 0, 85, 82, 100, 67, 73, 77, 0]
+
+# Bucket grades by decile, but put 100 in with the 90s
+histogram = Counter(min(grade // 10 * 10, 90) for grade in grades)
+
+plt.bar([x + 5 for x in histogram.keys()],  # Shift bars right by 5
+        histogram.values(),                 # Give each bar its correct height
+        10,                                 # Give each bar a width of 8
+        edgecolor=(0, 0, 0))                # Black edges for each bar
+
+plt.axis([-5, 105, 0, 5])                  # x-axis from -5 to 105,
+                                           # y-axis from 0 to 5
