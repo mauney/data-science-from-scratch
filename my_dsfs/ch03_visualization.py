c8fe25ba836462aed9465f536f42aafd75798431
mauney <15823766+mauney@users.noreply.github.com>
2020-04-25 23:24:06 -0400

    Viz y-axis

--- my_dsfs/ch03_visualization.py
+++ my_dsfs/ch03_visualization.py
@@ -55,6 +55,25 @@ plt.ylabel("# of Students")
 plt.title("Distribution of Exam 1 Grades")
 plt.show()
 
-
 # plt.savefig('im/viz_grades.png')
+plt.gca().clear()
+
+
+mentions = [500, 505]
+years = [2017, 2018]
+
+plt.bar(years, mentions, 0.8)
+plt.xticks(years)
+plt.ylabel("# of times I heard someone say 'data science'")
+
+# if you don't do this, matplotlib will label the x-axis 0, 1
+# and then add a +2.013e3 off in the corner (bad matplotlib!)
+plt.ticklabel_format(useOffset=False)
+
+# misleading y-axis only shows the part above 500
+plt.axis([2016.5, 2018.5, 499, 506])
+plt.title("Look at the 'Huge' Increase!")
+# plt.show()
+
+plt.savefig('im/viz_misleading_y_axis.png')
 plt.gca().clear()
